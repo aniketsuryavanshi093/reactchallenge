446f21ebf36842d45ddc5ab70b442cda87f0a06e
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2021-04-15 18:38:31 +0530

    First commit

---
+++ src/components/Messages/initialmessage.js
@@ -0,0 +1,9 @@
+import INITIAL_BOTTY_MESSAGE from '../../common/constants/initialBottyMessage';
+// v../../../common/constants/initialBottyMessage
+const ME = 'me';
+const BOT = 'bot';
+export const INITIAL_MESSAGE = {
+    message: INITIAL_BOTTY_MESSAGE,
+    id: Date.now(),
+    user: BOT
+  };
